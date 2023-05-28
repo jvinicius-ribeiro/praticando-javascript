868cc648637a9b300e6af3df5921db2467bb9db3
Vinicius Ribeiro <jvinicius.ribeiro@proton.me>
2023-05-27 22:08:11 -0300

    minor refactoring in the code

---
+++ 04-tipos-primitivos/index.js
@@ -0,0 +1,21 @@
+// string, representa um dado do tipo texto. Utilizado sempre com aspas. 
+let nome = "Vinicius";
+let mensagem = "Olá, mundo!";
+
+// number, como o nome sugere, representa dados numéricos, podendo ser inteiro ou floats.
+let preco = 12.666;
+let idade = 25;
+
+//bolean, usado para valores lógicos, validações de verdadeiro ou falso.
+let usuarioVerificado = false;
+
+if(usuarioVerificado){
+    console.log("Usuário verificado");
+}else{
+    console.log("Usuário não é verificado");
+}
+
+//tipo null, ou " vazio "
+let pessoa = null;
+
+//há também o undefined que geralmente é aplicado pelo próprio JS. 
---
+++ 05-operadores-logicos/index.js
@@ -0,0 +1,11 @@
+/*
+const a = "3"
+const b = 3
+
+console.log(a === b);
+*/
+
+const a = "letra"
+const b = "letra"
+
+console.log(a !== b)
