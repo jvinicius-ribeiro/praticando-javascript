c63a9f4947f070a5f15d44c218d0b2c5c7cbddeb
Vinicius Ribeiro <jvinicius.ribeiro@proton.me>
2023-06-03 17:47:53 -0300

    refatoração simples no código

--- 04-tipos-primitivos/index.js
+++ 04-tipos-primitivos/index.js
@@ -1,21 +1,21 @@
-// string, representa um dado do tipo texto. Utilizado sempre com aspas. 
-let nome = "Vinicius";
-let mensagem = "Olá, mundo!";
+// string = representa um dado do tipo texto. Utilizado sempre com aspas.
+const nome = 'Vinicius';
+const mensagem = 'Olá, mundo!';
 
-// number, como o nome sugere, representa dados numéricos, podendo ser inteiro ou floats.
+// number = como o nome sugere, representa dados numéricos, podendo ser inteiro ou floats.
 let preco = 12.666;
 let idade = 25;
 
-//bolean, usado para valores lógicos, validações de verdadeiro ou falso.
+//bolean = usado para valores lógicos, validações de verdadeiro ou falso.
 let usuarioVerificado = false;
 
-if(usuarioVerificado){
-    console.log("Usuário verificado");
-}else{
-    console.log("Usuário não é verificado");
+if (usuarioVerificado) {
+  console.log("Usuário verificado");
+} else {
+  console.log("Usuário não é verificado");
 }
 
-//tipo null, ou " vazio "
+//tipo null = ou " vazio "
 let pessoa = null;
 
-//há também o undefined que geralmente é aplicado pelo próprio JS. 
+//há também o undefined que geralmente é aplicado pelo próprio JS.
